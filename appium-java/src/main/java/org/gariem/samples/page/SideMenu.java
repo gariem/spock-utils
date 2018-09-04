d81a2754b26903bbfa8b5cea3dd0caa9fa31acbc
Emilio Garcia <raphsoft@gmail.com>
2018-09-04 16:15:28 -0500

    add scroll behaviour

--- appium-java/src/main/java/org/gariem/samples/page/SideMenu.java
+++ appium-java/src/main/java/org/gariem/samples/page/SideMenu.java
@@ -1,8 +1,10 @@
 package org.gariem.samples.page;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.concurrent.TimeUnit;
@@ -20,8 +22,22 @@ public class SideMenu extends BasePage {
 
     @Step("Click on the settings option")
     public void clickOnSettingsOption() throws InterruptedException {
+
+        boolean isFoundElement = driver.findElements(settingsOption).size() > 0;
+        while (isFoundElement == false) {
+            swipeVertical((AppiumDriver) driver, 0.9, 0.1, 0.5, 2000);
+            isFoundElement = driver.findElements(settingsOption).size() > 0;
+        }
         TimeUnit.SECONDS.sleep(2);
-        wait.until(ExpectedConditions.elementToBeClickable(settingsOption)).click();
+        driver.findElement(settingsOption).click();
+    }
+
+    public static void swipeVertical(AppiumDriver driver, double startPercentage, double finalPercentage, double anchorPercentage, int duration)  {
+        Dimension size = driver.manage().window().getSize();
+        int anchor = (int) (size.width * anchorPercentage);
+        int startPoint = (int) (size.height * startPercentage);
+        int endPoint = (int) (size.height * finalPercentage);
+        new TouchAction(driver).press(anchor, startPoint).waitAction(duration).moveTo(anchor, endPoint).release().perform();
     }
 
 }
